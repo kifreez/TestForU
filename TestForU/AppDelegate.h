83e3c6029a56c0fe7fee90e28ac87be2e0761e18
Anton Kriat <kifreez@gmail.com>
2017-02-27 15:50:37 +0200

    Initial Commit

---
+++ TestForU/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestForU
+//
+//  Created by Kif on 27.02.17.
+//  Copyright © 2017 KifApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
